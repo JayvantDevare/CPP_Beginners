03ecf05bc441c92964370e85b8bc5dac16446f47
JayvantDevare <jayvant.devare@gmail.com>
2018-07-19 16:55:49 +0530

    CPP Program for writing Destructor

---
+++ Program12.cpp
@@ -0,0 +1,62 @@
+//CPP Program for writing destructor
+//destructor is a member function of a class which free memory allocated for objects
+//destructor has same name as the class itself
+//denstructor don't have return type and parameters 
+//destructor is automatically get called when program execution is over
+
+
+#include<iostream>  //Header file declaration
+
+using namespace std;
+
+
+class FirstClass	//declaration of class 
+{
+	//data members
+	
+	public:
+			
+		string name;
+		int rollno;
+		
+		FirstClass()	//default constructor
+		{
+			name = "jayvant";
+			rollno = 100;
+		}
+		
+		~ FirstClass()	//destructor
+		{
+			cout<<"\n Destructor is get called";
+		}
+		
+	   	void displayData();	// function definition inside class
+
+	
+};   // end of class
+
+//writting functions outside the class
+
+		
+		
+void FirstClass :: displayData()     // displayData() function is called outside class function  - it is declared outside class
+		{
+			
+			cout<<"\t\nYour Name	:"<<name;
+			
+			cout<<"\n\tYour RollNo	:"<<rollno;
+			
+		}//end of display() function
+
+
+
+int main()  //main function
+{
+	
+	FirstClass ob;   // creation of objects automatically calls default constructor;
+	
+	ob.displayData(); // calling of displayData() display data which is initialized by default constructor
+	
+	return 0; 	//returning int value- main function has return type as integer
+	
+}	// end of main function
